68e4b089ac18194a21810428ee1554d0c05cf8fd
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-28 09:54:04 +0200

    Timer Example

--- app/src/main/java/com/sevenminuteworkout/MainActivity.kt
+++ app/src/main/java/com/sevenminuteworkout/MainActivity.kt
@@ -2,11 +2,22 @@ package com.sevenminuteworkout
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_exercise.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Variable for Timer which will be initialized later
+    private var countDownTimer: CountDownTimer? = null
+    // The duration of the timer in milliseconds
+    private var timeDuration: Long = 60000
+    // pauseOffset = timeDuration - time left
+    private var pauseOffset: Long = 0
+
 
     /**
      * This method is auto created by Android when the Activity Class is created.
@@ -16,6 +27,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // This is used to align the xml view to this class
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        tvTimer.text = "${(timeDuration/1000).toString()}"
+        btnStart.setOnClickListener {
+            startTimer(pauseOffset)
+        }
+        btnPause.setOnClickListener {
+            pauseTimer()
+        }
+        btnStop.setOnClickListener {
+            resetTimer()
+        }
 
         // Click event for start Button which we have created in XML.
         llStart.setOnClickListener {
@@ -25,5 +47,28 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             //END
         }
+        private fun startTimer(pauseOffsetL: Long) {
+            countDownTimer = object : CountDownTimer(timerDuration - pauseOffsetL, 1000) {
+                override fun onTick(millisUnitFinished: Long) {
+                    pauseOffset = timerDuration - millisUnitFinished
+                    tvTimer.text = (millisUnitFinished / 1000).toString()
+                    TODO("Not yet implemented")
+                }
+                override fun onFinish() {
+                    Toast.makeText(this@MainActivity, "Timer is finished", Toast.LENGTH_SHORT).show()
+                }
+            }.start()
+        }
+        private fun pauseTimer() {
+            if (countDownTimer != null) {
+                countDownTimer!!.cancel()
+            }
+        }
+        private fun resetTimer() {}
+        if(countDownTimer!!.cancel()
+                cuntDownTimer!!.cencel()
+            tvTimer.text = ${(timerDuaration/1000).toString()}
+        countDownTimer = null
+        pauseOffsetOffSet = 0
     }
 }
