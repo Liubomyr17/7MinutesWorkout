d1f9251a0b6f9f2eaf920477f343f856ed997e29
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2020-12-30 11:45:35 +0200

    Displaying At Witch Exercise We Currently Are

--- app/src/main/java/com/sevenminuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/sevenminuteworkout/ExerciseActivity.kt
@@ -17,9 +17,9 @@ import kotlin.collections.ArrayList
 class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
 
     private var restTimer: CountDownTimer? =
-        null // Variable for Rest Timer and later on we will initialize it.
+            null // Variable for Rest Timer and later on we will initialize it.
     private var restProgress =
-        0 // Variable for timer progress. As initial the rest progress is set to 0. As we are about to start.
+            0 // Variable for timer progress. As initial the rest progress is set to 0. As we are about to start.
 
     private var exerciseTimer: CountDownTimer? = null // Variable for Exercise Timer and later on we will initialize it.
     private var exerciseProgress = 0 // Variable for exercise timer progress. As initial the exercise progress is set to 0. As we are about to start.
@@ -31,11 +31,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
 
     private var player: MediaPlayer? = null // Created a varible for Media Player to use later.
 
-    // TODO(Step 1 : Declaring a variable of an adapter class to bind it to recycler view.)
-    // START
-    // Declaring an exerciseAdapter object which will be initialized later.
+    // Declaring a exerciseAdapter object which will be initialized later.
     private var exerciseAdapter: ExerciseStatusAdapter? = null
-    // END
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,11 +51,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
 
         setupRestView() // REST View is set in this function
 
-        // TODO(Step 3 : Calling the function where we have bound the adapter to recycler view to show the data in the UI.)
-        // START
         // setting up the exercise recycler view
         setupExerciseStatusRecyclerView()
-        // END
     }
 
     /**
@@ -118,7 +112,7 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
          */
         try {
             val soundURI =
-                Uri.parse("android.resource://com.sevenminuteworkout/" + R.raw.press_start)
+                    Uri.parse("android.resource://com.sevenminuteworkout/" + R.raw.press_start)
             player = MediaPlayer.create(applicationContext, soundURI)
             player!!.isLooping = false // Sets the player to be looping or non-looping.
             player!!.start() // Starts Playback.
@@ -167,11 +161,17 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
                 restProgress++ // It is increased to ascending order
                 progressBar.progress = 10 - restProgress // Indicates progress bar progress
                 tvTimer.text =
-                    (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
+                        (10 - restProgress).toString()  // Current progress is set to text view in terms of seconds.
             }
 
             override fun onFinish() {
                 currentExercisePosition++
+
+                // TODO(Step 1 : When we are getting an updated position of exercise set that item in the list as selected and notify the adapter class.)
+                // START
+                exerciseList!![currentExercisePosition].setIsSelected(true) // Current Item is selected
+                exerciseAdapter!!.notifyDataSetChanged() // Notified the current item to adapter class to reflect it into UI.
+                // END
 
                 setupExerciseView()
             }
@@ -222,14 +222,21 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
             }
 
             override fun onFinish() {
+
+                // START
+                exerciseList!![currentExercisePosition].setIsSelected(false) // exercise is completed so selection is set to false
+                exerciseList!![currentExercisePosition].setIsCompleted(true) // updating in the list that this exercise is completed
+                exerciseAdapter!!.notifyDataSetChanged() // Notifying the adapter class.
+                // END
+
                 if (currentExercisePosition < 11) {
                     setupRestView()
                 } else {
 
                     Toast.makeText(
-                        this@ExerciseActivity,
-                        "Congratulations! You have completed the 7 minutes workout.",
-                        Toast.LENGTH_SHORT
+                            this@ExerciseActivity,
+                            "Congratulations! You have completed the 7 minutes workout.",
+                            Toast.LENGTH_SHORT
                     ).show()
                 }
             }
@@ -237,29 +244,26 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener  {
     }
 
     /**
-     * Function is used to speak the text that we pass to it.
+     * Function is used to speak the text what we pass to it.
      */
     private fun speakOut(text: String) {
         tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     /**
-     * Function is used to set up the recycler view to UI and asining the Layout Manager and Adapter Class is attached to it.
+     * Function is used to set up the recycler view to UI and assing the Layout Manager and Adapter Class is attached to it.
      */
-    // TODO(Step 2 : Binding adapter class to recycler view and setting the recycler view layout manager and passing a list to the adapter.)
-    // START
     private fun setupExerciseStatusRecyclerView() {
 
-        // Defining a layout manager for the recycle view
-        // Here we have used a LinearLayout Manager with horizontal scroll.
+        // Defining a layout manager to recycle view
+        // Here we have used Linear Layout Manager with horizontal scroll.
         rvExerciseStatus.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+                LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        // As the adapter expects the exercises list and context so initialize it passing it.
+        // As the adapter expect the exercises list and context so initialize it passing it.
         exerciseAdapter = ExerciseStatusAdapter(exerciseList!!, this)
 
         // Adapter class is attached to recycler view
         rvExerciseStatus.adapter = exerciseAdapter
     }
-    // END
 }
--- app/src/main/java/com/sevenminuteworkout/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/sevenminuteworkout/ExerciseStatusAdapter.kt
@@ -1,9 +1,11 @@
 package com.sevenminuteworkout
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_exercise_status.view.*
 
@@ -41,8 +43,21 @@ class ExerciseStatusAdapter(val items: ArrayList<ExerciseModel>, val context: Co
         val model: ExerciseModel = items[position]
 
         holder.tvItem.text = model.getId().toString()
-    }
 
+        if(model.getIsSelected()) {
+            holder.tvItem.background =
+                    ContextCompat.getDrawable(context, R.drawable.item_circular_thin_color_accent_border)
+            holder.tvItem.setTextColor(Color.parseColor("#212121"))
+        } else if(model.getIsCompleted()) {
+            holder.tvItem.background =
+                    ContextCompat.getDrawable(context, R.drawable.item_circular_color_accent_background)
+        holder.tvItem.setTextColor(Color.parseColor("#FFFFFF"))
+    } else {
+            holder.tvItem.background =
+                    ContextCompat.getDrawable(context, R.drawable.item_circular_color_gray_background)
+            holder.tvItem.setTextColor(Color.parseColor("#212121"))
+        }
+    }
     /**
      * Gets the number of items in the list
      */
